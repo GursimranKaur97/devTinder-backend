1f880181f32c065298de4dc88603c2f920ed505b
GursimranKaur97 <gursimrankaur1664@gmail.com>
2025-04-25 17:24:23 +0530

    multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -1,24 +1,69 @@
-const express = require('express');
+const express = require("express");
+const app = express();
 
-const app = express()
+app.use("/user", [
+  (req, res, next) => {
+    // Route Handler 1
+    console.log("Handling the route user!!");
+    next();
+    // res.send('1st Response!!');
+  },
+  (req, res, next) => {
+    // Route Handler 2
+    console.log("Handling the route user!!");
+    // res.send('2nd Response!!');
+    next();
+  },
+  (req, res, next) => {
+    // Route Handler 3
+    console.log("Handling the route user!!");
+    // res.send('3rd Response!!');
+    next();
+  },
+  (req, res, next) => {
+    // Route Handler 4
+    console.log("Handling the route user!!");
+    // res.send('4th Response!!');
+    next();
+  },
+  (req, res) => {
+    // Route Handler 5
+    console.log("Handling the route user!!");
+    res.send("5th Response!!");
+  },
+]);
+// It'll give response for / abc and also for /ac as by add ? after b it becomes optional
+// app.get('/ab?c', (req,res)=>{
+//     res.send({firstName: 'Gursimran', lastName: 'Kaur'})
+// })
 
+// It'll give response for / abcd and also for /abcbcd or / abcbcbcd
+// app.get('/a(bc)+d', (req,res)=>{
+//     res.send({firstName: 'Gursimran', lastName: 'Kaur'})
+// })
+
+// it'll give response for dragonfly or dragonnnnnnnflag but not given any response for dragonflyjj
+app.get(/.fly$/, (req, res) => {
+  res.send({ firstName: "Gursimran", lastName: "Kaur" });
+});
 // This will only handle GET call to /user
-app.get('/user', (req,res)=>{
-    res.send({firstName: 'Gursimran', lastName: 'Kaur'})
-})
+app.get("/user/:userId/:name/:password", (req, res) => {
+  console.log("***req.params****", req.params);
+  res.send({ firstName: "Gursimran", lastName: "Kaur" });
+});
 
-app.post('/user', (req, res) => {
-    console.log('Save Data to the database');
-    res.send('Data successfully saved to the database');
-})
+app.post("/user", (req, res) => {
+  console.log("Save Data to the database");
+  res.send("Data successfully saved to the database");
+});
 
-app.delete('/user', (req,res)=>{
-res.send('Deleting successfully')
-})
+app.delete("/user", (req, res) => {
+  res.send("Deleting successfully");
+});
 // This will match all the HTTP method API calls to /test so if we do GET or POST call for all it'll return us Hello form the server
-app.use('/test',(req,res)=>{
-    res.send('Hello from the server')
-})
-app.listen(3030, ( )=>{
-    console.log('Server successfully listening on port 3030')
+app.use("/test", (req, res) => {
+  res.send("Hello from the server");
+});
+app.listen(3030, () => {
+  console.log("Server successfully listening on port 3030");
 });
