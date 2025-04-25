459fb6f28b02d10fa928238094d26056a65a7b4f
GursimranKaur97 <gursimrankaur1664@gmail.com>
2025-04-25 14:07:50 +0530

    exploring get, post, delete

--- src/app.js
+++ src/app.js
@@ -2,15 +2,22 @@ const express = require('express');
 
 const app = express()
 
-app.use('/',(req,res)=>{
-    res.send('Hello from the dashboard')
-})
-app.use('/test',(req,res)=>{
-    res.send('Hello from the server')
+// This will only handle GET call to /user
+app.get('/user', (req,res)=>{
+    res.send({firstName: 'Gursimran', lastName: 'Kaur'})
 })
 
-app.use('/hello',(req,res)=>{
-    res.send('Hello hello hello')
+app.post('/user', (req, res) => {
+    console.log('Save Data to the database');
+    res.send('Data successfully saved to the database');
+})
+
+app.delete('/user', (req,res)=>{
+res.send('Deleting successfully')
+})
+// This will match all the HTTP method API calls to /test so if we do GET or POST call for all it'll return us Hello form the server
+app.use('/test',(req,res)=>{
+    res.send('Hello from the server')
 })
 app.listen(3030, ( )=>{
     console.log('Server successfully listening on port 3030')
