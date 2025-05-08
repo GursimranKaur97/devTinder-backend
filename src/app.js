98ce18df06bd5968cb1ecd8d4ddfbd6905a6eb81
GursimranKaur97 <gursimrankaur1664@gmail.com>
2025-05-08 09:14:32 +0530

    schema validations

--- src/app.js
+++ src/app.js
@@ -61,6 +61,32 @@ app.get("/feed", async(req, res) => {
   }
 });
 
+// delete data from user
+app.delete('/user', async(req,res)=>{
+    const userId = req?.body?.userId;
+    try{
+        const user = await User.findByIdAndDelete(userId);
+        res.send('User deleted successfully')
+    } catch(err){
+        res.status(400).send('Something went wrong')
+    }
+})
+
+// update data
+app.patch('/user', async(req,res)=>{
+    const userId = req?.body?.userId;
+    const data = req.body;
+    try{
+        const user = await User.findByIdAndUpdate({_id: userId}, data, {
+            returnDocument: "after",
+            runValidators: true
+        });
+        res.send('User updated successfully')
+    } catch(err){
+        res.status(400).send('UPDATE FAILED:' + err?.message)
+    }
+})
+
 connectDB()
   .then(() => {
     console.log("Database connection established...");
