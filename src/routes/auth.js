08499f7f2f481ee7b53129f8213317ce4cd31229
GursimranKaur97 <gursimrankaur1664@gmail.com>
2025-11-11 11:31:05 +0530

    updated user code

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -22,8 +22,13 @@ authRouter.post('/signup', async(req, res)=>{
             password: passwordHash
         })
         
-        await user.save();
-        res.send("User added successfully");
+        const savedUser = await user.save();
+        const token = await savedUser.getJWT();
+
+        res.cookie("token", token, {
+            expires: new Date(Date.now() + 8*3600000) // Cookie will expire in 8 hours
+        });
+        res.json({message: "User added successfully", data: savedUser});
       } catch (err) {
         res.status(400).send("ERROR::" + err?.message);
       }
